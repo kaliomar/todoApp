26a56cdfe81a8a7aaf8d6181c6014e57eeb32218
kaliomar <kaliomar89@gmail.com>
2024-11-07 17:30:03 +0200

    h-w first working version with json file

--- rwfunc.py
+++ rwfunc.py
@@ -1,18 +1,32 @@
 import json
-import todoapp 
+import shared
 
 def re_file(): # to read todos from file
-    with open("vault.json","r") as remember:
-        magic = json.load(remember)
-    return magic
+    with open("vault.json","r") as ref:
+        json_dict = json.load(ref)
+    return json_dict
+
 
 def wr_file(): # to save todos to file
-    text = todoapp.todo()
-    if text == "file is empty in ":
-        print(text)
-    else:
-        with open("vault.json",mode="w",encoding = "utf-8") as wr_file:
-            json.dump(text,wr_file)
-        wr_file.close()
+    todos = re_file()
+    date = input("date: ")
+    mission = input("mission: ")
+
+    if date not in todos.keys(): # key isn't in file 
+        todos[date] = mission
+    
+    elif type(todos[date]) == list: # there are many todos on this date
+        li = []
+        for i in todos[date]:
+            li.append(i)
+        li.append(mission)
+        todos[date] = li
         
-wr_file()
+    else: # there is one mission on this date
+        li = [todos[date],mission]
+        todos[date] = li
+
+    with open("vault.json",mode="w") as wrf:
+        json.dump(todos, wrf)
+    wrf.close()
+
--- todoapp.py
+++ todoapp.py
@@ -1,67 +1,39 @@
 import json
-import os
 
-todol = {}
+def add_todo(): # this function for adding mission to json file
+    from rwfunc import wr_file
+    wr_file()
 
-def todo(): #this function for adding mission to dict
+
+def show_missions(): # to print out mission of specific date
     date = input("select date: ")
-    mission = input("mission: ")
-    with open("vault.json") as mem:
-        context = mem.read()
-    if len(context) > 0:
-        todol = json.load(mem)
-        print("file is big")
-    else:
-        todol = {date:mission}
-        print("file is empty")
-    mem.close()
+    from rwfunc import re_file
+    current_missions = re_file()
 
-    if date not in todol.keys(): # add mission to new dates
-        todol[date] = mission
-        print("type 1")
-        return todol
-    
-    else: # add mission to current existance date
-        if type(todol[date]) == list: # there are many todos on this date
-            li = []
-            for i in todol[date]:
-                li.append(i)
-            li.append(mission)
-            todol[date] = li
-            print("type 2")
-            return todol
-        
-        else: # there is one mission on this date
-            li = [todol[date],mission]
-            todol[date] = li
-            print("type 3")
-            return todol
-
-def output(): # to print out mission of specific date
-    date = input("select date: ")
-    if date not in todol.keys(): # user date isn't in our dict
+    if date not in current_missions.keys(): # user date isn't in our dict
         print("not existance")
     
     else: # user date is in our list
-        if type(todol[date]) == list: # but date has many mission on it
-            for i in todol[date]:
+        if type(current_missions[date]) == list: # but date has many mission on it
+            for i in current_missions[date]:
                 print(i)
-        
         else: # date has one mission on it 
-            print(todol[date])
+            print(current_missions[date])
 
-def asking(): # to ask if user want to add or check date and mission
-    which = input("Add new todo select 'i':\nCheck mission of date select 'o':\nType your selection: ")
-    numof = int(input("How many times do you need to try: "))
-    if which == 'i': # if user choose to add
-        for i in range(numof):
-            todo()
-    elif which == 'o': # if user choose to check
-        for i in range(numof):
-            output()
+
+def choosing(): # to ask if user want to add or check date and mission
+    print("          **** type (a) for adding new mission or (s) to show missions on specific date **** ")
+    add_or_show = input("Type your selection: ")
+    times_to_do = int(input("How many times do you need to try: "))
+    if add_or_show == 'a': # if user choose to add
+        for i in range(times_to_do):
+            add_todo()
+    elif add_or_show == 's': # if user choose to check
+        for i in range(times_to_do):
+            show_missions()
     else: # if user is a stupid asshole 
         print("not allowed choice")
 
 
 
-todo()
+choosing()
